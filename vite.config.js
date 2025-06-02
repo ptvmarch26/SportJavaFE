76253599c1ab8f22fd7793113eea0cf7e556048a
ptvmarch26 <vovanphithong263@gmail.com>
2025-06-02 20:31:46 +0700

    update vercel json

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd())
-
-  return {
-    plugins: [react()],
-    base: env.VITE_BASE_PATH || '/',
-  }
-})
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "dist", 
+  },
+  base: "/",
+});
